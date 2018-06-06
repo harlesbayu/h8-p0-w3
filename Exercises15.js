3a98d6f0dd9c3e068a183512db64031eef69e1d9
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2018-06-06 23:27:36 +0700

    Last Commit

--- Exercises15.js
+++ Exercises15.js
@@ -31,47 +31,31 @@ console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'c
 // Solve Problem
 function groupAnimals(animals) {
   // you can only write your code here!
-  var sortAnimals = animals;
-  var kumpulan = []
+  var groups = []
 
   for(var i = 0; i < animals.length; i++){
-    firstWord[i] = animals[i][0]
+    var isGroup = true
+    var group = []
+
+    for(var j = 0; j < groups.length; j++){
+      if(groups[j][0][0] === animals[i][0]){
+        groups[j].push(animals[i])
+        isGroup = false
+      }
+    }
+
+    if(isGroup){
+      group.push(animals[i])
+      groups.push(group)
+    }
   }
 
-  console.log(firstWord);
-  
-  for(var k = 0; k < sortAnimals.length; k++){
-    var isGroup = true;
-    var group = [];
-    
-    for(var j = 0; j < kumpulan.length; j++){
-      //console.log("test test")
-       if(sortAnimals[k][0] === kumpulan[j][0][0]) {
-         kumpulan[j].push(sortAnimals[k])
-         //console.log("sudah ad group")
-         isGroup = false;
-         
-       }
-    }
-    
-    //console.log(isGroup);
-    if(isGroup) {
-      group.push(sortAnimals[k])
-    
-      kumpulan.push(group)
-    }
-    console.log(kumpulan);
-    //break;
-    
-    
-    
-  }
+  return groups
 
- // console.log(kumpulan)
 }
 
 // TEST CASES
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa','cicak', 'kancil']));
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 //console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
