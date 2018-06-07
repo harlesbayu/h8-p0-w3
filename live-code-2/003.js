4845e5f0fc4459a19bce69fb113fcb7d388f3a0e
Harles Bayu Anggara <harlesbayu@DESKTOP-0I587HV.localdomain>
2018-06-07 14:29:42 +0700

    Last Commit

---
+++ live-code-2/003.js
@@ -0,0 +1,44 @@
+/*
+================
+Break Sentence
+================
+Name :_____________
+
+[INSTRUCTIONS]
+breakSentence adalah sebuah function yang menerima satu parameter berupa string kalimat.
+function akan mereturn sebuah array yang berisikan setiap kata di kalimat tesebut.
+
+[RULE]
+- Wajib menuliskan pseudocode. Tidak ada pseudocode / pseudocode tidak match dengan kode maka indikasi soal tidak diselesaikan secara pribadi dan tidak akan dinilai.
+- Hanya boleh menggunakan sintaks for/while, if-else, serta operasi array untuk pemecahan masalah.
+- Dilarang menggunakan sintaks .split()! Jangan lupa, split bukanlah operasi array.
+- Dilarang menggunakan regex .match dan lainnya!
+
+[EXAMPLE]
+breakSentence('I am so amazed')
+output: ['i', 'am', 'so', 'amazed']
+*/
+
+function breakSentence(sentence) {
+  // only write code here
+  var groups = []
+  var group = ""
+  for(var i = 0; i < sentence.length; i++){
+   if(sentence[i] !== " "){
+     group += sentence[i] 
+   }
+   else if(sentence[i] == " "){
+     groups.push(group)
+     group = ""
+   }
+   
+  }
+  groups.push(group)
+  return groups 
+}
+
+console.log(breakSentence('I am so amazed')); // ['I', 'am', 'so', 'amazed' ]
+console.log(breakSentence('do something')); // ['do', 'something']
+console.log(breakSentence('')); // []
+console.log(breakSentence('week3 is so easy')); // ['week3', 'is', 'so', 'easy']
+console.log(breakSentence('I can do this with my eyes closed')); // ['I', 
